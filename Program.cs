f3ac014502d43fd7cba9f6ab00da445b2f0c315e
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-13 08:29:04 +0200

    first init

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _09.WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            // calculate n! = 1 * 2 * 3 .... n;
+            int result = 1;
+
+            while (true)
+            {
+                if (n == 1)
+                {
+                    break;
+                }
+                result *= n;
+                n--;
+            }
+            Console.WriteLine("n! = {0}", result);
+
+        }
+    }
+}
